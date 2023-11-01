e959f51ac62e0576d8a01837cce2a9e8a845f0a4
Kevin <kevin923.dai@gmail.com>
2023-10-31 23:42:49 -0500

    fix strassen more

--- cpu/cpu_stass.h
+++ cpu/cpu_stass.h
@@ -13,42 +13,42 @@ mat cpu_strass(mat &A, mat &B, int n) {
 
     int split_size = n / 2;
 
-    mat a = split(n, A, 0, 0);
-    mat b = split(n, A, 0, split_size);
-    mat c = split(n, A, split_size, 0);
-    mat d = split(n, A, split_size, split_size);
+    mat a = split(n, A, 0, 0); // A11
+    mat b = split(n, A, 0, split_size); // A12
+    mat c = split(n, A, split_size, 0); // A21
+    mat d = split(n, A, split_size, split_size); // A22
 
-    mat e = split(n, B, 0, 0);
-    mat f = split(n, B, 0, split_size);
-    mat g = split(n, B, split_size, 0);
-    mat h = split(n, B, split_size, split_size);
+    mat e = split(n, B, 0, 0); // B11
+    mat f = split(n, B, 0, split_size); // B12
+    mat g = split(n, B, split_size, 0); // B21
+    mat h = split(n, B, split_size, split_size); // B22
 
     mat s1;
     #pragma omp task shared(s1)
     {
         mat fh_sub = addsub_matricies(split_size, f, h, false);
-        s1 = cpu_strass(a, fh_sub, split_size);
+        s1 = cpu_strass(a, fh_sub, split_size); // A11 * (B12 - B22)
     }
 
     mat s2;
     #pragma omp task shared(s2)
     {
         mat ab_add = addsub_matricies(split_size, a, b, true);
-        s2 = cpu_strass(ab_add, h, split_size);
+        s2 = cpu_strass(ab_add, h, split_size); // (A11 + A12) * B22
     }
 
     mat s3;
     #pragma omp task shared(s3)
     {
         mat cd_add = addsub_matricies(split_size, c, d, true);
-        s3 = cpu_strass(cd_add, e, split_size);
+        s3 = cpu_strass(cd_add, e, split_size); // (A21 + A22) * B11
     }
 
     mat s4;
     #pragma omp task shared(s4)
     {
         mat ge_sub = addsub_matricies(split_size, g, e, false);
-        s4 = cpu_strass(d, ge_sub, split_size);
+        s4 = cpu_strass(d, ge_sub, split_size); // A22 * (B21 - B11)
     }
 
     mat s5;
@@ -56,8 +56,7 @@ mat cpu_strass(mat &A, mat &B, int n) {
     {
         mat ad_add = addsub_matricies(split_size, a, d, true);
         mat eh_add = addsub_matricies(split_size, e, h, true);
-        s5 = cpu_strass(ad_add, eh_add, split_size);
-
+        s5 = cpu_strass(ad_add, eh_add, split_size); // (A11 + A22) * (B11 + B22)
     }
 
     mat s6;
@@ -65,8 +64,7 @@ mat cpu_strass(mat &A, mat &B, int n) {
     {
         mat bd_sub = addsub_matricies(split_size, b, d, false);
         mat gh_add = addsub_matricies(split_size, g, h, true);
-        s6 = cpu_strass(bd_sub, gh_add, split_size);
-
+        s6 = cpu_strass(bd_sub, gh_add, split_size); // (A12 - A22) * (B21 + B22)
     }
 
     mat s7;
@@ -74,7 +72,7 @@ mat cpu_strass(mat &A, mat &B, int n) {
     {
         mat ac_sub = addsub_matricies(split_size, a, c, false);
         mat ef_add = addsub_matricies(split_size, e, f, true);
-        s7 = cpu_strass(ac_sub, ef_add, split_size);
+        s7 = cpu_strass(ac_sub, ef_add, split_size); // (A11 - A21) * (B11 + B12)
     }
 
 
@@ -83,29 +81,29 @@ mat cpu_strass(mat &A, mat &B, int n) {
     mat c11;
     #pragma omp task shared(c11)
     {
-        mat s5_s4_add = addsub_matricies(split_size, s5, s4, true);
-        mat s2_s6_add = addsub_matricies(split_size, s2, s6, true);
-        c11 = addsub_matricies(split_size, s5_s4_add, s2_s6_add, false);
+        mat s5_s4_add = addsub_matricies(split_size, s5, s4, true); // S5 + S4
+        mat s2_s6_sub = addsub_matricies(split_size, s2, s6, false); // S2 - S6
+        c11 = addsub_matricies(split_size, s5_s4_add, s2_s6_sub, false); // S5 + S4 - (S2 - S6) = S5 + S4 + S6 - S2
     }
 
     mat c12;
     #pragma omp task shared(c12)
     {
-        c12 = addsub_matricies(split_size, s1, s2, true);
+        c12 = addsub_matricies(split_size, s1, s2, true); // S1 + S2
     }
 
     mat c21;
     #pragma omp task shared(c21)
     {
-        c21 = addsub_matricies(split_size, s3, s4, true);
+        c21 = addsub_matricies(split_size, s3, s4, true); // S3 + S4
     }
 
     mat c22;
     #pragma omp task shared(c22)
     {
-        mat s1_s5_add = addsub_matricies(split_size, s1, s5, true);
-        mat s3_s7_sub = addsub_matricies(split_size, s3, s7, false);
-        c22 = addsub_matricies(split_size, s1_s5_add, s3_s7_sub, true);
+        mat s5_s1_add = addsub_matricies(split_size, s5, s1, true); // S5 + S1
+        mat s3_s7_sub = addsub_matricies(split_size, s3, s7, true); // S3 + S7
+        c22 = addsub_matricies(split_size, s5_s1_add, s3_s7_sub, false); // S5 + S1 - (S3 + S7) = S5 + S1 - S3 - S7
     }
 
     #pragma omp taskwait
