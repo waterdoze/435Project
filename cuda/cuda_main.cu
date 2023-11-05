5e03095d32031d081e2efd8f8df61f8de619c934
Matthew Wang <mattwang@Matthews-MacBook-Air-8.local>
2023-11-04 19:05:32 -0500

    finished a majority of naive just need to add cali

--- cuda/cuda_main.cu
+++ cuda/cuda_main.cu
@@ -14,7 +14,17 @@
 int THREADS;
 int BLOCKS;
 int NUM_VALS;
-int 
+
+
+
+void fillMatrix(int n, int*& mat)
+{
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            mat[i * n + j] = rand() % 5;
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
     /*
@@ -27,14 +37,40 @@ int main(int argc, char *argv[]) {
 
     //So we know we have threads amount of threads so can we calculate how blocks we need then?
 
-    BLOCKS = MATRIX_SIZE;
+    BLOCKS = MATRIX_SIZE * MATRIX_SIZE / THREADS;
+
+    
+
     //When we pass in the thread count it's talking about
     /*
     Issues that it's threads per block
     Essentially each block will take in threads amount of rows and cols and then send them to each block
     thus there will be (n*n)/threads blocks with thread threads inside of it
+
+    steps for cuda naive
+    make the array 1d(so change the way we populate)
+    when we send the array we only have to send over once and then use the blocking to index the correct ones
     */
-    int *d_A, *d_B, *d_C;
-    cudaMalloc((void**)&d_A,)
+    
+    
+
+
+
+    adiak::init(NULL);
+    adiak::launchdate();                                         // launch date of the job
+    adiak::libraries();                                          // Libraries used
+    adiak::cmdline();                                            // Command line used to launch the job
+    adiak::clustername();                                        // Name of the cluster
+    adiak::value("Algorithm", "MPI Naive Matrix Multiplication");// The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
+    adiak::value("ProgrammingModel", "MPI");          // e.g., "MPI", "CUDA", "MPIwithCUDA"
+    adiak::value("Datatype", int);                          // The datatype of input elements (e.g., double, int, float)
+    adiak::value("SizeOfDatatype", sizeof(int));              // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
+    adiak::value("InputSize", N);                        // The number of elements in input dataset (1000)
+    // adiak::value("InputType", inputType);                        // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
+    adiak::value("num_procs", size);                        // The number of processors (MPI ranks)
+    // adiak::value("num_threads", num_threads);                    // The number of CUDA or OpenMP threads
+    // adiak::value("num_blocks", num_blocks);                      // The number of CUDA blocks
+    adiak::value("group_num", 8);                     // The number of your group (integer, e.g., 1, 10)
+    adiak::value("implementation_source", "Online");
     return 0;
 }
--- cuda/cuda_naive.h
+++ cuda/cuda_naive.h
@@ -1,14 +1,37 @@
 #ifndef CUDA_NAIVE
 #define CUDA_NAIVE
 
-__global__ void matrixMultiplication(int* mat1, int* mat2, int* product, int n)
+__global__ void matrixMultiplication(int* rowsA, int* matrixB, int* rowsC, int n)
 {
     int prod = blockIdx.x * blockDim.x + threadIdx.x;
     int i = prod / n;
     int j = prod % n;
     for (int k = 0; k < n; k++) {
-        product[i * n + j] += mat1[i * n + k] * mat2[k * n + j];
+        rowC[i * n + j] += mat1[i * n + k] * mat2[k * n + j];
     }
+}
+
+void naive(int* matrixA, int* matrixB,int* matrixC,int matSize){
+    size_t bytes = n * n * sizeof(int)
+
+    size_t bytes = n * n * sizeof(int);
+
+    int *d_mat1, *d_mat2, *d_product;
+
+    cudaMalloc(&d_mat1, bytes);
+    cudaMalloc(&d_mat2, bytes);
+    cudaMalloc(&d_product, bytes);
+
+    cudaMemcpy(d_mat1, h_mat1, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_mat2, h_mat2, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_product, h_product, bytes, cudaMemcpyHostToDevice);
+
+    int* rowsA[matSize/threads][matSize];
+    int* rowsC[matSize/threads][matSize];
+
+    dim3 blocks(BLOCKS,1);
+    dim3 threads(THREADS,1);
+
 }
 
 #endif
