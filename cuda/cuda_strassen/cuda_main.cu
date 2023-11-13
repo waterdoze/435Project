284669f5eb46d94889c8bfcdabcfdd2ea6568dee
Kevin <kevin923.dai@gmail.com>
2023-11-13 14:32:35 -0600

    update adiak for cuda_strass

--- cuda/cuda_strassen/cuda_main.cu
+++ cuda/cuda_strassen/cuda_main.cu
@@ -581,11 +581,11 @@ int main(int argc, char *argv[])
     adiak::libraries();                                            // Libraries used
     adiak::cmdline();                                              // Command line used to launch the job
     adiak::clustername();                                          // Name of the cluster
-    adiak::value("Algorithm", "CUDA Naive Matrix Multiplication"); // The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
+    adiak::value("Algorithm", "CUDA Strass Matrix Multiplication"); // The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
     adiak::value("ProgrammingModel", "CUDA");                      // e.g., "MPI", "CUDA", "MPIwithCUDA"
     adiak::value("Datatype", "int");                               // The datatype of input elements (e.g., double, int, float)
     adiak::value("SizeOfDatatype", sizeof(int));                   // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
-    adiak::value("InputSize", MATRIX_SIZE);                        // The number of elements in input dataset (1000)
+    adiak::value("InputSize", n);                        // The number of elements in input dataset (1000)
     // adiak::value("InputType", inputType);                        // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
     // adiak::value("num_procs", size);                        // The number of processors (MPI ranks)
     adiak::value("num_threads", THREADS); // The number of CUDA or OpenMP threads
